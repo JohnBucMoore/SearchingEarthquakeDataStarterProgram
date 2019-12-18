f815c29bf21ced550f83659447613aa0f94787f6
John Buchanan Moore <johnbucmoore@gmail.com>
2019-12-18 11:46:46 -0500

    add indexOfLargest method

--- LargestQuakes.java
+++ LargestQuakes.java
@@ -1,20 +1,38 @@
 import java.util.*;
 public class LargestQuakes {
-    public void findLargestQuakes() {
+    public ArrayList<QuakeEntry> getData() {
         EarthQuakeParser parser = new EarthQuakeParser();
         String source = "data/nov20quakedatasmall.atom";
         //String source = "data/nov20quakedata.atom";
         //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
-        ArrayList<QuakeEntry> list  = parser.read(source);
+        ArrayList<QuakeEntry> list = parser.read(source);
+        return list;
+    }
+
+    public void findLargestQuakes() {
+        ArrayList<QuakeEntry> list = getData();
+        /*
         for (QuakeEntry quake : list) {
             System.out.println(quake);
         }
+         */
         System.out.println("read data for "+list.size());
 
     }
 
+    public int indexOfLargest(ArrayList<QuakeEntry> data) {
+        int dex = 0;
+        for (int i = 1; i < data.size(); i++) {
+            if (data.get(i).getMagnitude() > data.get(dex).getMagnitude()) {
+                dex = i;
+            }
+        }
+        return dex;
+    }
+
     public void test() {
         findLargestQuakes();
+        //System.out.println(indexOfLargest(getData()));
     }
 
     public static void main(String[] args) {
